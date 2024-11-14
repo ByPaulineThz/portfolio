3278718d2b00ba6906af0667eada2138c75aa8e6
ByPaulineThz <thelliezpauline@gmail.com>
2024-11-14 22:14:54 +0100

    First Commit

---
+++ header.php
@@ -0,0 +1,25 @@
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!--FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+
+    <!--STYLE-->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+
+    <script>
+      new WOW().init();
+    </script>
+  
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Portfolio | Pauline Thz</title>
+<?php include 'header.php'?>>
+</head> 
+  <body>
+    <h1>Welcome to my UX UI Portfolio</h1>
+    <p>I'm Pauline Thz</p>
+
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="js/style.js"></script>
+  </body>
+</html>
