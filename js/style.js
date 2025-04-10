e75bd21216634b87da43e2ab32e5589318cf8a69
ByPaulineThz <thelliezpauline@gmail.com>
2025-04-10 20:53:14 +0200

    Maj

--- js/style.js
+++ js/style.js
@@ -1,15 +1,5 @@
-fetch("./header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
-
-fetch("./footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
+const toggleButton = document.querySelector('.toggle-menu');
+const navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', () => {
+  navBar.classList.toggle('toggle');
+});
