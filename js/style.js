03a84817e4a05f086599baccb46cfb4234c96faa
ByPaulineThz <thelliezpauline@gmail.com>
2025-04-15 10:29:03 +0200

    update style project

--- js/style.js
+++ js/style.js
@@ -3,3 +3,31 @@ const navBar = document.querySelector('.nav-bar');
 toggleButton.addEventListener('click', () => {
   navBar.classList.toggle('toggle');
 });
+
+// swiper    
+var mySwiper = new Swiper('.swiper-container', {
+  effect: '',
+  loop: false,
+  speed: 1000,
+  slidesPerView: 1,
+  navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+     el: '.swiper-pagination',
+     type: 'bullets',
+     clickable: 'true'
+  },
+
+});
+       
+       
+var swiper = new Swiper(".myGallery", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
