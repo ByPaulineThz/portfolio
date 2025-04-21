68c9746c4af2fdcf04765835eb341cd3e6c01020
ByPaulineThz <thelliezpauline@gmail.com>
2025-04-21 20:15:10 +0200

    persona

--- js/style.js
+++ js/style.js
@@ -31,3 +31,18 @@ var swiper = new Swiper(".myGallery", {
     hide: true,
   },
 });
+
+
+function openUserInfos(e, elementName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(elementName).style.display = "block";
+  e.currentTarget.className += " active";
+}
